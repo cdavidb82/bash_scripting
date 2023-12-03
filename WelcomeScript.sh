2802b5bc9b2ba0deaf9b48c7f4b405bbcf8fea4e
David <cdavidb82@outlook.com>
2023-12-02 20:27:14 -0500

    Initial Commit

---
+++ WelcomeScript.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+# This script will ask for user input, placve the input in a varible, and
+# display the varible contents in string.
+
+echo "What is your name?"
+
+read name
+
+echo "What chapter are you on in Linix Basics for Hackers?"
+
+read chapter
+
+echo "Welcome $name to Chapter $chapter of Linux Basic for Hackers!"
+
